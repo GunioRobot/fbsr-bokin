4ab879521b345c9e1746ab194fd059fc88be463c
Axel Tomasson <axel.tomasson@gmail.com>
2009-12-04 22:05:47 +0000

    laga passa

--- fbsr/settings.py
+++ fbsr/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'fbsr'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'fbsr'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'fbsr'         # Not used with sqlite3.
+DATABASE_PASSWORD = 'strumpa'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
